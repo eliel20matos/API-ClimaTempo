4ec930485d1a0235bb31c3084bf248d161a9b2e3
juniordsj <juniorronaldodsj@gmail.com>
2021-07-14 19:59:23 -0400

    teste

--- src/main/java/br/com/eliel/repository/HigrometroRepository.java
+++ src/main/java/br/com/eliel/repository/HigrometroRepository.java
@@ -2,8 +2,13 @@ package br.com.eliel.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.eliel.domain.Anemometro;
 import br.com.eliel.domain.Higrometro;
 
 public interface HigrometroRepository extends JpaRepository<Higrometro, Long>{
+	
+	Anemometro findByUuid(String uuid);
+	
+	void deleteByUuid(String uuid);
 
 }
